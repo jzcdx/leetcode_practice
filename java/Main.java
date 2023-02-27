4b3066cdf32b3eb10e157cd6ab08b2867b51a3b3
MicroOptimization <jackiezhen538@gmail.com>
2023-02-27 18:24:29 -0500

    meant to say, did leetcode 1 and 217

--- java/Main.java
+++ java/Main.java
@@ -1,24 +1,14 @@
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
-        /*
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        */
-        System.out.println("hi");
-        int temp[] = null;
-
-        int nums[] = new int[]{1, 2, 3, 4, 5};
-        int target = 3;
+        System.out.println("hi globe");
     }
 
     public static int[] twoSum(int[] nums, int target) {
         int res[] = new int[2];
         HashMap<Integer, Integer> visited = new HashMap<Integer, Integer>();
-        //visited.put(3, 5);
         for (int i = 0; i < nums.length; i++) {
             int curNum = nums[i];
             int compliment = target - curNum;
@@ -44,6 +34,6 @@ public class App {
     }
 
     public static boolean validAnagram(String s, String t) {
-        
+        return false;
     }
 }
